a44ef3fd79c0f3b71fd80a010e5b3a65037b9e96
lijian <ljhhh@qq.com>
2021-02-25 22:56:58 +0800

    使用idea的内置工具httpclient,分别进行了get和post请求的测试。

--- src/main/java/com/lijian/wiki/controller/TestController.java
+++ src/main/java/com/lijian/wiki/controller/TestController.java
@@ -3,14 +3,28 @@ package com.lijian.wiki.controller;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @ComponentScan("com.lijian")
 @RestController
 public class TestController {
 
-    @RequestMapping("hello")
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
     public String hello(){
         return "helloworld";
     }
+
+    @RequestMapping(value = "hello-post",method = RequestMethod.POST)
+    public String helloPost(){
+        return "helloworld-post";
+    }
+
+    @RequestMapping(value = "hello-post-param",method = RequestMethod.POST)
+    public String helloPostParam(String name){
+        return "helloworld-post-param" + name;
+    }
 }
+
