0b935ab2def784205ea8fd34c45ecfa6715fc112
lijian <ljhhh@qq.com>
2021-02-25 23:16:32 +0800

    1.springboot默认识别properties和yml 2.springcloud中默认识别propertiesth和yml还支持bootstrap.properties/yml 3.学习了读取springboot配置文件的方法

--- src/main/java/com/lijian/wiki/controller/TestController.java
+++ src/main/java/com/lijian/wiki/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.lijian.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.Map;
 
@@ -12,9 +14,13 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    //如果配置文件有值则取配置文件中的值，没有则取默认值，这里是defaultValue
+    @Value("${test.hello:defaultValue}")
+    private String testHello;
+
     @RequestMapping(value = "hello",method = RequestMethod.GET)
     public String hello(){
-        return "helloworld";
+        return "helloworld " +testHello;
     }
 
     @RequestMapping(value = "hello-post",method = RequestMethod.POST)
