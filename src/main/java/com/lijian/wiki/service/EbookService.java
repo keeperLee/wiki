aa7c7a0d154e1fb39a57b22c897d6c7d04c41db0
lijian <ljhhh@qq.com>
2021-03-04 22:21:08 +0800

    列表查询接口支持动态sql name参数有值时按name查，没值时查全部 封面小图样式调整

--- src/main/java/com/lijian/wiki/service/EbookService.java
+++ src/main/java/com/lijian/wiki/service/EbookService.java
@@ -10,6 +10,7 @@ import com.lijian.wiki.response.EbookResp;
 import com.lijian.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -24,7 +25,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+req.getName()+"%");
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%"+req.getName()+"%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> respList = new ArrayList<>();
