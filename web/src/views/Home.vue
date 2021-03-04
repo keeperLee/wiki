514e4b4ee7bf8be55cb6f00bed275088f1fc9107
lijian <ljhhh@qq.com>
2021-03-04 23:27:30 +0800

    修改axios的baseUrl读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -98,7 +98,7 @@ export default defineComponent({
 
     onMounted(()=>{
       console.log("onMounted")
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((res)=>{
+      axios.get("/ebook/list").then((res)=>{
         const data = res.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
